421b505be0baa67f0509618d8dd1cecf0c54fe9a
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2022-06-25 10:19:20 -0400

    update

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCjUsMAe7XcXM6AyVKh3RYwFxIN8Z1Re10",
+  authDomain: "daily-counter-app.firebaseapp.com",
+  projectId: "daily-counter-app",
+  storageBucket: "daily-counter-app.appspot.com",
+  messagingSenderId: "574740419747",
+  appId: "1:574740419747:web:f9fcf9d1699c8d214a1b16",
+  measurementId: "G-9Y4MGXBYMT"
+};
